6f44da981e07b21b41c4a72a19e7f1d7e5f66e05
Saint <krogstat@gmail.com>
2020-09-07 13:33:52 +0300

    Initial commit

---
+++ src/ru/mirea/inbo0519/pr1/HelloJava.java
@@ -0,0 +1,9 @@
+package ru.mirea.inbo0519.pr1;
+import java.lang.System;
+import java.util.*;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
